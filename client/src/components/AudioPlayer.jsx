9566200a6750b529676c2bbacd0c46eda8001a9d
Aleksi Kuusinen <aleksi.kuusinen@aalto.fi>
2024-12-27 22:11:46 +0200

    Small adjustments and removal of obsolete parts

--- client/src/components/AudioPlayer.jsx
+++ client/src/components/AudioPlayer.jsx
@@ -1,47 +1,13 @@
-import { Howl } from "howler"
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import fileService from "../services/files"
 
 
 
 const AudioPlayer = ({audioName}) =>{ 
 
-  const disableAudioButton = true //Debugging purposes
-
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [sound, setSound] = useState(null)
-
   const [loaded, setLoaded] = useState(false)
   const [audioURL, setAudioURL] = useState("")
 
-  useEffect(() => {
-    const newSound = new Howl({
-      src: [audioName],
-      html5: true,
-      onend: () => {
-        // Automatically set state to not playing when the sound ends
-        setIsPlaying(false);
-      },
-    })
-    setSound(newSound);
-    return () => {
-      newSound.unload();
-    };
-  }, [audioName]);
-
-  const playSound = () => {
-    if (sound) {
-      sound.play()
-      setIsPlaying(true)
-    }
-  }
-
-  const stopSound = () => {
-    if (sound) {
-      sound.stop()
-      setIsPlaying(false)
-    }
-  }
   const toggleLoaded = async () =>{
     const newURL = await fileService.getFileURL(audioName)
     setAudioURL(newURL)
@@ -51,11 +17,6 @@ const AudioPlayer = ({audioName}) =>{
   /* Dunno which one is better, html simple and easy but howler offers more functionality?? */
   return (
     <div>
-      { !disableAudioButton &&
-        <button onClick={isPlaying ? stopSound : playSound}>
-          {isPlaying ? "Stop Sound" : "Play Sound"}
-        </button>
-      }
     { !loaded ? (
         <button onClick={toggleLoaded}> Load audio </button>
     ) : (
