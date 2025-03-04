5d83e74a1b8cdf325e4e14ce649469ded188d5b6
Oliver <olihniemi@gmail.com>
2025-03-04 21:28:44 +0200

    Minor refactoring

--- client/src/components/AudioPlayer.jsx
+++ client/src/components/AudioPlayer.jsx
@@ -4,19 +4,17 @@ import Button from 'react-bootstrap/Button';
 
 
 const AudioPlayer = ({ audioName }) => {
-  const [loaded, setLoaded] = useState(false);
-  const [audioURL, setAudioURL] = useState("");
+  const [audioURL, setAudioURL] = useState(null);
 
   const toggleLoaded = async () => {
     const newURL = await fileService.getFileURL(audioName);
     setAudioURL(newURL);
-    setLoaded(true);
   };
 
   /* Dunno which one is better, html simple and easy but howler offers more functionality?? */
   return (
     <div>
-      {!loaded ? (
+      {!audioURL ? (
         <Button style={{ marginBottom: '0.25rem'}} variant="primary" onClick={toggleLoaded}> Load audio </Button>
       ) : (
         <audio controls>
