120cbbfd6084841a4af3a0b7a6429187d7f9cab5
Oliver <olihniemi@gmail.com>
2025-03-07 16:44:53 +0200

    Navigation styling and placeholder for searchbar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const Navigation = () => {
   return (
-    <div>
-      <a href="/" >Home </a>
-      <a href="/users" >Users </a>
-      <a href="/entries" >Entries </a>
-    </div>
+    <Navbar className='bg-body-tertiary' sticky='top'>
+      <Container fluid>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Navbar.Brand href="/">
+            <img src='../lanturn.png' width={40} height={40}></img>
+          </Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href="/" >Home </Nav.Link>
+            <Nav.Link href="/users" >Users </Nav.Link>
+            <Nav.Link href="/entries" >Entries </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
